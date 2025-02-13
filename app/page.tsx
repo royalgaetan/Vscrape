c4b09c7714d5f812cf018a6f3533a30df01ad753
royalgaetan <gaetanroyalpro@gmail.com>
2025-02-14 00:01:36 +0400

    Remove comments

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,29 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-full w-full bg-gray-900">
-      <div className=" text-white p-6">
-        {/* Animated Image */}
+    <div
+      className="flex flex-col items-center justify-center h-full w-full bg-gray-900"
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100%",
+        width: "100%",
+      }}
+    >
+      <div
+        className=" text-white p-6"
+        style={{
+          padding: "10vh",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "auto",
+          width: "auto",
+        }}
+      >
         <div>
           <Image
             src="/underConstruction.png"
@@ -17,14 +37,12 @@ export default function Home() {
           />
         </div>
 
-        {/* Animated Text */}
         <h1 className="text-2xl sm:text-4xl font-bold">
           🚧 Site Under Construction 🚧
         </h1>
 
-        {/* Subtext */}
         <p className="mt-3 text-gray-400 text-center">
-          We’re cooking something amazing. Stay tuned!
+          😋 We’re cooking something amazing. Stay tuned! 👌
         </p>
       </div>
     </div>
